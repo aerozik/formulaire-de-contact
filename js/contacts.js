28548a5f33c3cc2df78df7a1c544307b26e4c2b6
aerozik <lenumero20@gmail.com>
2016-01-05 00:09:54 +0100

    ajout de la boucle du programme js

--- js/contacts.js
+++ js/contacts.js
@@ -36,3 +36,30 @@ function choixOptions() {
 
         console.log("Bienvenue dans le gestionnaire des contacts !");
         choixOptions();
+		
+		  //le programme va boucler tant que le choix de l 'utisateur ne sera pas 0 
+    while (choix !== 0) {
+         var choix = Number(prompt("saisissez votre choix :"));
+        if (choix ===1) {
+             console.log("Voici la liste de vos contact : ");
+     // ici je demande que le programme utilise la fonction décrire pour lister les contacts présentdans mon tableau
+            contactTab.forEach(function (contact) {
+            console.log(contact.decrire());    
+                });
+            choixOptions();            
+     } else if (choix === 2) {
+            var saisieNom = prompt("veuillez sasir un nouveau nom de contact :");
+            var saisiePrenom = prompt("veuillez saisir son prénom :");
+            nouveauContact = Object.create(Contact);
+            nouveauContact.init(saisieNom, saisiePrenom);
+            contactTab.push(nouveauContact);
+            console.log("Le nouveau contact a été ajouté avec succès");
+                    
+      } else if (choix === 0) {
+          console.log("Merci d'avoir utilisé le gestionnaire de contact, au revoir.");
+      } else {
+          console.log("Désolé, " + choix + " n'est pas une option valide");
+      }
+       
+       
+    }  
